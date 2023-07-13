2ea1b2eb8a117e78b23c781b7a24907e93659544
amac81 <arnaldocanelas@gmail.com>
2023-07-13 14:18:19 +0100

    CORS config

---
+++ src/main/java/pt/bitclinic/AcGameList/config/WebConfig.java
@@ -0,0 +1,29 @@
+package pt.bitclinic.AcGameList.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+	//Cross-Origin Resource Sharing (CORS)
+	//https://supertokens.com/blog/what-is-cross-origin-resource-sharing
+	
+	
+	@Value("${cors.origins}")
+	private String corsOrigins;
+
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("*").allowedOrigins(corsOrigins);
+			}
+		};
+	}
+	
+}
