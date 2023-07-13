3207058ddd3acdb85e427581ca023f28c1856620
amac81 <arnaldocanelas@gmail.com>
2023-07-13 12:41:20 +0100

    Homologation ok

--- src/main/java/pt/bitclinic/AcGameList/projections/GameMinProjection.java
+++ src/main/java/pt/bitclinic/AcGameList/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package pt.bitclinic.AcGameList.projections;
 public interface GameMinProjection {
 	Long  getId();
 	String getTitle();
-	Integer getYear();
+	Integer getGameYear();
 	String getImgUrl();
 	String getShortDescription();
 	Integer getPosition();
